1a91dbf1baec2c152357da6839a115645b765460
Vincent Yu <yyu@haverford.edu>
2020-07-17 12:59:20 -0400

    Update model_params.py

--- model_params.py
+++ model_params.py
@@ -10,20 +10,20 @@ common_params = {
     'meta': False,
     'full_dataset': True,
     'fine_tuning': True,
-    'with_historical_data': True,   # Train models with historical data of other amines
-    'with_k': False,     # Train the model with k additional amine-specific experiments
-    'train_size': 10,   # k after pretrain
+    'with_historical_data': True,  # Train models with historical data of other amines
+    'with_k': False,  # Train the model with k additional amine-specific experiments
+    'train_size': 10,  # k after pretrain
     'active_learning': False,
-    'active_learning_iter': 10,   # x before active learning
+    'active_learning_iter': 10,  # x before active learning
     'stats_path': Path('./results/cv_statistics.pkl'),
-    'cv_stats_overwrite': True,     # TODO: change this to false when running all models
+    'cv_stats_overwrite': True,  # TODO: change this to false when running all models
     'save_model': False
 }
 
 knn_configs = {
     'category_3': {
         'n_neighbors': 3,
-        'leaf_size': 30,
+        'leaf_size': 1,
         'p': 1
     },
     'category_4_i': {
@@ -33,18 +33,18 @@ knn_configs = {
     },
     'category_4_ii': {
         'n_neighbors': 3,
-        'leaf_size': 30,
+        'leaf_size': 1,
         'p': 1
     },
     'category_5_i': {
-        'n_neighbors': 3,
-        'leaf_size': 30,
+        'n_neighbors': 1,
+        'leaf_size': 1,
         'p': 1
     },
     'category_5_ii': {
-        'n_neighbors': 3,
-        'leaf_size': 30,
-        'p': 1
+        'n_neighbors': 1,
+        'leaf_size': 1,
+        'p': 3
     },
 }
 
@@ -55,26 +55,29 @@ knn_params = {
 }
 
 svm_configs = {
-    'category_3': {
-
-    },
-    'category_4_i': {
-
-    },
-    'category_4_ii': {
-
-    },
-    'category_5_i': {
-
-    },
-    'category_5_ii': {
-
-    },
+    'category_3': {},
+    'category_4_i': {},
+    'category_4_ii': {},
+    'category_5_i': {},
+    'category_5_ii': {}
 }
 
 svm_params = {
     'configs': svm_configs,
     'model_name': 'SVM'
+}
+
+linearsvm_configs = {
+    'category_3': {},
+    'category_4_i': {},
+    'category_4_ii': {},
+    'category_5_i': {},
+    'category_5_ii': {}
+}
+
+linearsvm_params = {
+    'configs': linearsvm_configs,
+    'model_name': 'LinearSVM'
 }
 
 randomforest_params = {
@@ -89,25 +92,48 @@ logisticregression_params = {
 
 dt_configs = {
     'category_3': {
-
+        'criterion': 'gini',
+        'splitter': 'best',
+        'max_depth': 7,
+        'min_samples_split': 2,
+        'min_samples_leaf': 2
     },
     'category_4_i': {
-
+        'criterion': 'gini',
+        'splitter': 'random',
+        'max_depth': 11,
+        'min_samples_split': 8,
+        'min_samples_leaf': 1,
+        'class_weight': {0: 0.3, 1: 0.7}
     },
     'category_4_ii': {
-
+        'criterion': 'gini',
+        'splitter': 'best',
+        'max_depth': 3,
+        'min_samples_split': 4,
+        'min_samples_leaf': 1
     },
     'category_5_i': {
-
+        'criterion': 'gini',
+        'splitter': 'random',
+        'max_depth': 11,
+        'min_samples_split': 4,
+        'min_samples_leaf': 3,
+        'class_weight': {0: 0.1, 1: 0.9}
     },
     'category_5_ii': {
-
+        'criterion': 'gini',
+        'splitter': 'best',
+        'max_depth': 4,
+        'min_samples_split': 2,
+        'min_samples_leaf': 1
     },
 }
 
 decisiontree_params = {
     'configs': dt_configs,
-    'model_name': 'Decision_Tree'
+    'model_name': 'Decision_Tree',
+    'visualize': False
 }
 
 gradientboosting_params = {
