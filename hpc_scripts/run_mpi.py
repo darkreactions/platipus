cf1924df5e313510c53ed96e7d47600970f45e3c
Venkateswaran Shekar <vshekar@login3.stampede2.tacc.utexas.edu>
2020-07-18 18:25:37 -0500

    Sucessful message passing. TODO: Integrate models

--- hpc_scripts/run_mpi.py
+++ hpc_scripts/run_mpi.py
@@ -2,6 +2,7 @@
 from mpi4py import MPI
 from hpc_scripts.hpc_params import common_params, meta_params, meta_train, meta_test
 from pathlib import Path
+import numpy as np
 
 from models.meta.platipus_class import Platipus
 from models.meta.init_params import init_params
@@ -15,9 +16,9 @@ import time
 
 nn_configs = [(200, 100, 100), (400, 300, 200), (200, 100), (400, 300)]
 # TAGS
-SEND_NEXT_PARAM = 0
-PARAM = 1
-TERMINATE = 2
+SEND_NEXT_PARAM = 100
+PARAM = 101
+TERMINATE = 102
 
 
 def new_platipus(params):
@@ -39,38 +40,63 @@ def new_platipus(params):
 
 
 if __name__ == "__main__":
-    node_num = sys.argv[1]
+    node_num = 1 
     comm = MPI.COMM_WORLD
     rank = comm.Get_rank()
     status = MPI.Status()
-
+    all_params = get_all_params()
     print(f'My rank is: {rank}')
+    
     if rank == 0:
-        all_params = get_all_params()
+        # all_params = get_all_params()
         current_param = 0
-
-        while current_param < len(all_params):
-            msg = comm.recv(source=MPI.ANY_SOURCE, tag=SEND_NEXT_PARAM,
-                            status=status)
+        block_len = len(all_params)/3
+        print(f'Block length = {block_len}')
+        start = int(block_len*node_num)
+        end = int(block_len*(node_num+1))
+        param_block = [i for i in range(start, end)]
+        while current_param < len(param_block):
+            print(f'Rank {rank} : Waiting for message')
+            #msg = comm.recv(source=MPI.ANY_SOURCE, tag=MPI.ANY_TAG, status=status)
+            data = np.empty(1, dtype='i')
+            comm.Recv(data, source=MPI.ANY_SOURCE, tag=MPI.ANY_TAG, status=status)
+            print(f'Rank {rank} : Message received {data}')
             dest = status.Get_source()
-            param = all_params[current_param]
-            comm.send(param, dest=dest, tag=PARAM)
+            param_block_num = param_block[current_param]
+            print(f'Rank {rank}: Selected param_block_num {param_block_num} to send')
+            #comm.send(param_block_num, dest=dest, tag=PARAM)
+            data = np.array([param_block_num], dtype='i')
+            comm.Send([data, MPI.INT], dest=dest, tag=PARAM)
             current_param += 1
         process = 0
         while process < 4:
-            msg = comm.recv(source=MPI.ANY_SOURCE, tag=SEND_NEXT_PARAM,
-                            status=status)
+            data = np.empty(1, dtype='i')
+            comm.Recv(data, source=MPI.ANY_SOURCE, tag=MPI.ANY_TAG, status=status)
+            #msg = comm.recv(source=MPI.ANY_SOURCE, tag=SEND_NEXT_PARAM,
+            #                status=status)
             dest = status.Get_source()
-            comm.send(1, dest=dest, tag=TERMINATE)
+            #comm.send(1, dest=dest, tag=TERMINATE)
+            data = np.array([0], dtype='i')
+            comm.Send([data, MPI.INT], dest=dest, tag=TERMINATE)
             process += 1
 
     else:
-        comm.send(1, dest=0, tag=SEND_NEXT_PARAM)
-        msg = comm.recv(source=0, tag=MPI.ANY_TAG, status=status)
+        print(f'Rank {rank} sending NEXT PARAM message') 
+        data = np.array([1], dtype='i')
+        #comm.send(1, dest=0, tag=SEND_NEXT_PARAM)
+        comm.Send([data, MPI.INT], dest=0, tag=SEND_NEXT_PARAM)
+        data = np.empty(1, dtype='i')
+        comm.Recv(data, source=0, tag=MPI.ANY_TAG, status=status)
         tag = status.Get_tag()
+        print(f'Rank {rank} got {data} with tag {tag}')
         while tag != TERMINATE:
-            print(f'Param on rank {rank}: {msg}')
+            print(f'Param on rank {rank}: {data} : {all_params[data[0]]} ')
+            
             time.sleep(1)
-            comm.send(1, dest=0, tag=SEND_NEXT_PARAM)
-            msg = comm.recv(source=0, tag=MPI.ANY_TAG, status=status)
+            #comm.send(1, dest=0, tag=SEND_NEXT_PARAM)
+            comm.Send([data, MPI.INT], dest=0, tag=SEND_NEXT_PARAM)
+            #msg = comm.recv(source=0, tag=MPI.ANY_TAG, status=status)
+            data = np.empty(1, dtype='i')
+            comm.Recv(data, source=0, tag=MPI.ANY_TAG, status=status)
             tag = status.Get_tag()
+    
